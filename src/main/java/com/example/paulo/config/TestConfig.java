b1b133a014328ac2f08b15e60423481d903caefe
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-02-09 22:33:36 -0300

    Payment, one-to-one association

--- src/main/java/com/example/paulo/config/TestConfig.java
+++ src/main/java/com/example/paulo/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.paulo.entities.Category;
 import com.example.paulo.entities.Order;
 import com.example.paulo.entities.OrderItem;
+import com.example.paulo.entities.Payment;
 import com.example.paulo.entities.Product;
 import com.example.paulo.entities.User;
 import com.example.paulo.entities.enums.OrderStatus;
@@ -85,7 +86,13 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-
+		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		
+		orderRepository.save(o1);
 	}
 
 }
--- src/main/java/com/example/paulo/entities/User.java
+++ src/main/java/com/example/paulo/entities/User.java
@@ -18,6 +18,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
