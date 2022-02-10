3179422d7764370a2bca628352aaf028ee1c43de
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-02-09 21:59:43 -0300

    Product-Order-Item one-to-many association

--- src/main/java/com/example/paulo/entities/Product.java
+++ src/main/java/com/example/paulo/entities/Product.java
@@ -11,8 +11,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
@@ -39,7 +42,8 @@ public class Product implements Serializable{
 	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
-	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {
 	}
@@ -120,9 +124,19 @@ public class Product implements Serializable{
 	public Set<Category> getCategories() {
 		return categories;
 	}
+	
+	
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		
+		return set;
+	}
 
-
-
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -131,8 +145,7 @@ public class Product implements Serializable{
 		return result;
 	}
 
-
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
