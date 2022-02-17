80c58e8c59e28cb2cf52fc061d49305530ce1860
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-02-17 15:46:04 -0300

    Exception handling - delete

--- src/main/java/com/example/paulo/services/UserService.java
+++ src/main/java/com/example/paulo/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.paulo.entities.User;
 import com.example.paulo.repositories.UserRepository;
+import com.example.paulo.services.exceptions.DatabaseException;
 import com.example.paulo.services.exceptions.ResourceNotFoundException;
 
 
@@ -35,7 +38,13 @@ public class UserService {
 	
 	//Deletar do banco o usuário
 	public void delete(Long id) {
-		repository.deleteById(id);;
+		try {
+			repository.deleteById(id);	
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);		
+			} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+			}
 	}
 	
 	
