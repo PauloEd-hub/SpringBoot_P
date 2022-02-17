2f1315e53600aeb0503f3c276ca814aa960d24c8
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-02-17 14:54:35 -0300

    Exception Handler - findById

--- src/main/java/com/example/paulo/services/UserService.java
+++ src/main/java/com/example/paulo/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.paulo.entities.User;
 import com.example.paulo.repositories.UserRepository;
+import com.example.paulo.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -24,7 +25,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional <User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		
 	}
 	
